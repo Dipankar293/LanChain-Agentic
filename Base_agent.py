0a88c1aa75494e3fdf1397b252e0125eedeb17aa
Dipankar Borah <dipborah293@gmail.com>
2026-04-09 22:59:08 +0530

    Basic agent development Done

--- Base_agent.py
+++ Base_agent.py
@@ -1,4 +1,37 @@
 
 from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
 
-agent = create_agent("openai:gpt-5")
+
+# Basic agent creation
+agent_basic = create_agent("openai:gpt-5")
+
+basic_response = agent_basic.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in NE India"}]}
+)
+
+print("FULL RESPONSE:", basic_response)
+
+#Customised agent creation (ChatAnthropic, ChatGoogleGenerativeAI)
+model = ChatOpenAI(
+    model="gpt-5",
+    temperature=0.9,
+    max_tokens=10000,
+    timeout=30
+)
+
+agent_custom_model = create_agent(model)
+
+response = agent_custom_model.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in NE India"}]}
+)
+
+
+#get the content of the last message in the response
+print("FULL RESPONSE:", response["messages"][-1].content)
+
+#get the token usage of the last message in the response
+print("TOKEN USAGE:", response["messages"][-1].response_metadata["token_usage"])
+
+#get the message id of the last message in the response
+print("MESSAGE ID:", response["messages"][-1].response_metadata["id"])
